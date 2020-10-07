aabf221974225d7b9950fa95b4aeb46c7923c857
Jean Philip <jrogatis@conecta.net.br>
2020-10-07 10:51:52 -0300

    add bucket s3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { projectRouter } from '../api/projects';
 import { postsRouter } from '../api/posts/';
 import { Application } from 'express';
 import { usersRouter } from '../api/user';
+import { imageGalleryRouter } from '../api/imageGallery';
 const router = (app: Application): void => {
   app.use('/api/projects', projectRouter);
   app.use('/api/posts', postsRouter);
   app.use('/api/users', usersRouter);
+  app.use('/api/imageGallery', imageGalleryRouter);
 };
 
 export default router;
